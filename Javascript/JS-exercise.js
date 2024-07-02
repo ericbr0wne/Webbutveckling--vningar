47b32f2280313929f62d2bcc724b11eb0c484dc9
ericbr0wne <ericbrowne@hotmail.com>
2024-07-02 22:22:56 +0200

    added 99 bottles js code

--- Javascript/JS-exercise.js
+++ Javascript/JS-exercise.js
@@ -11,7 +11,27 @@ function addNumbers(num) {
 }
 
 //Exercise2
-
+for (let bottles = 99; bottles > 0; bottles--) {
+    if (bottles === 1) {
+      console.log(
+        `${bottles} bottle of beer on the wall, ${bottles} bottle of beer.`
+      );
+      console.log(
+        `Take one down and pass it around, no more bottles of beer on the wall.`
+      );
+    } else {
+      console.log(
+        `${bottles} bottles of beer on the wall, ${bottles} bottles of beer.`
+      );
+      console.log(
+        `Take one down and pass it around, ${bottles - 1} bottles of beer on the wall.`
+      );
+    }
+    console.log(""); // Add an empty line between verses
+  }
+  
+  console.log("No more bottles of beer on the wall, no more bottles of beer.");
+  console.log("Go to the store and buy some more, 99 bottles of beer on the wall.");
 
 //Exercise3
 
